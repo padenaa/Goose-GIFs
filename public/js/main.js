1921b5fe72a477b508e66aec9a3df9aa2f748934
padenaa <padenarasouli@gmail.com>
2021-09-06 16:50:48 +0000

    made frontend for the website

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+const getRandom = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener("click", (e) => {
+  //let myKey = process.env.secretkey;
+  let myKey = '';
+  let myQuery = `https://api.giphy.com/v1/gifs/search?api_key=${myKey}&q=goose`;
+  fetch(myQuery)
+    .then((response)=> response.json()) 
+    .then(json => {
+      let gif = json.data[getRandom(json.data.length)];
+      imageHolderDiv.innerHTML = "<img src=" + gif.images.original.url + ">"; 
+  })
+})
